ba46bc93604ea8ff6c8be03e2855b622ade4f774
Daniel King <daniel.king5@nhs.net>
2017-12-02 13:26:47 +0000

    day 1 part 2

--- day1.rb
+++ day1.rb
@@ -1,7 +1,11 @@
 class Captcha
+  def initialize(rotate_steps = 1)
+    @steps = rotate_steps
+  end
+
   def solve(sequence_string)
     sequence = sequence_string.split(//)
-    sequence.zip(sequence.rotate)
+    sequence.zip(sequence.rotate(@steps))
             .map{ |a,b| a == b ? a.to_i : 0 }
             .sum
   end
@@ -9,3 +13,6 @@ end
 
 solver = Captcha.new
 ["1122", "1111", "1234", "91212129"].each { |x| puts solver.solve(x) }
+
+["1212", "1221", "123425", "123123", "12131415"]
+  .each { |x| puts Captcha.new(x.length/2).solve(x) }
