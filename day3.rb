5a33fb944aa1ff4427db1467a5352b44938749ff
Daniel King <daniel.king5@nhs.net>
2017-12-04 23:07:25 +0000

    day 3 part 2

--- day3.rb
+++ day3.rb
@@ -31,5 +31,74 @@ class Spiral
   end
 end
 
-spiral = Spiral.new
-[1, 12, 23, 1024].each { |point| puts spiral.steps(point) }
+#spiral = Spiral.new
+#[1, 12, 23, 1024].each { |point| puts spiral.steps(point) }
+
+class SpiralAdjacent
+  def initialize(max_point)
+    @max_point = max_point
+
+    width = Math.sqrt(max_point).ceil
+    width = width + 1 if (width % 2 == 0)
+    @spiral = Array.new(width) { Array.new(width) { 0 } }
+
+    @y, @x = [width/2, width/2]
+    @spiral[@y][@x] = 1
+    @y, @x = [@y, @x+1]
+    @spiral[@y][@x] = 1
+    @direction = "up"
+  end
+
+  def draw_spiral
+    while @spiral[@y][@x] < @max_point
+      update_position
+      @spiral[@y][@x] = neighbours_total
+      # render
+    end
+
+    puts @spiral[@y][@x]
+  end
+
+  def neighbours_total
+    [@spiral[@y-1][@x-1],
+    @spiral[@y-1][@x+1],
+    @spiral[@y-1][@x],
+    @spiral[@y+1][@x-1],
+    @spiral[@y+1][@x+1],
+    @spiral[@y+1][@x],
+    @spiral[@y][@x-1],
+    @spiral[@y][@x+1]].sum
+  end
+
+  def update_position
+    case @direction
+    when "up"
+      @y = @y - 1
+      if @spiral[@y][@x-1] == 0
+        @direction = "left"
+      end
+    when "left"
+      @x = @x - 1
+      if @spiral[@y+1][@x] == 0
+        @direction = "down"
+      end
+    when "down"
+      @y = @y + 1
+      if @spiral[@y][@x+1] == 0
+        @direction = "right"
+      end
+    when "right"
+      @x = @x + 1
+      if @spiral[@y-1][@x] == 0
+        @direction = "up"
+      end
+    end
+  end
+
+  def render
+    system "clear"
+    puts @spiral.map { |x| x.join(' ') }
+  end
+end
+
+SpiralAdjacent.new(1024).draw_spiral
